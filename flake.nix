d9c4aacb5dc0e172a3558eb5194401d80d95b5ff
Samuel Kyletoft <samuel@kyletoft.se>
2026-05-10 21:23:40 +0200

    repo: nixify build
    flake: Only copy lisp files
    
    (I dunno, I just need another commit so that nix will accept the update)

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+		ghostty.url = "github:ghostty-org/ghostty/01825411ab2720e47e6902e9464e805bc6a062a1";
+	};
+
+	outputs = { self, nixpkgs, flake-utils, ghostty }: flake-utils.lib.eachDefaultSystem(system:
+	let pkgs = import nixpkgs { inherit system; };
+	in {
+		packages.default = pkgs.stdenv.mkDerivation rec {
+			pname = "ghostel";
+			version = "0.24.0";
+			src = ./.;
+			nativeBuildInputs = [ pkgs.zig_0_15 ];
+			buildInputs = [ ghostty.packages.${system}.libghostty-vt ];
+			patchPhase = ''
+				sed -i "s|GHOSTTY_NIX_PATH|${ghostty.packages.${system}.libghostty-vt}|g" build.zig
+			'';
+			buildPhase = ''
+				make build
+			'';
+			installPhase =
+				let real-out = "$out/share/emacs/site-lisp/elpa/ghostel-${version}";
+				in ''
+					mkdir -p ${real-out}
+					cp ghostel-module.so ${real-out}/libghostel-module.so
+					cp $src/symbols.map ${real-out}
+					cp $src/lisp/*.el ${real-out}
+					cp -r $src/vendor $src/etc ${real-out}
+				'';
+		};
+	});
+}
